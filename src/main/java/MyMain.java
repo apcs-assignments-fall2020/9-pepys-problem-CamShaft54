060eabd777430e6bf7056a28e1c8ebf8e2117565
camshaft54 <cam.shaw1234@gmail.com>
2020-10-01 11:11:37 -0700

    Add one-line version of each method (commented out)

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MyMain {
 
@@ -8,55 +10,51 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        Random rand = new Random();
         int count = 0;
-        for (int i = 0; i < 10000; i++) {
-            boolean x = false;
-            for (int j = 0; j < 6; j++) {
-                if (1+rand.nextInt(6) == 6)
-                    x = true;
-            }
-            if (x) {
-                count++;
+        int x = 0;
+        for (int i = 0; i < 60000; i++) {
+            if (new Random().nextInt(6) == 5) x++;
+            if (i % 6 == 0) {
+                if (x > 0) count++;
+                x = 0;
             }
         }
         return count/100d;
+        //The line below contains a one line version that may or may not work. It is not recommended to use this version due to extreme inefficiencies
+        //return IntStream.range(0, 60000).boxed().mapToInt(s -> (s % 6 == 0) ? ((Arrays.stream(Arrays.copyOfRange(IntStream.range(0, 60000).boxed().mapToInt(u -> (new Random().nextInt(6) == 5) ? 1 : 0).toArray(), s, s+6)).anyMatch(t -> t == 1)) ? 1 : 0) : 0).sum()/100d;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        Random rand = new Random();
         int count = 0;
-        for (int i = 0; i < 10000; i++) {
-            int x = 0;
-            for (int j = 0; j < 12; j++) {
-                if (1+rand.nextInt(6) == 6)
-                    x++;
-            }
-            if (x >= 2) {
-                count++;
+        int x = 0;
+        for (int i = 0; i < 120000; i++) {
+            if (new Random().nextInt(6) == 5) x++;
+            if (i % 12 == 0) {
+                if (x > 1) count++;
+                x = 0;
             }
         }
         return count/100d;
+        //The line below contains a one line version that may or may not work. It is not recommended to use this version due to extreme inefficiencies
+        //return IntStream.range(0, 120000).boxed().mapToInt(s -> (s % 6 == 0) ? ((Arrays.stream(Arrays.copyOfRange(IntStream.range(0, 120000).boxed().mapToInt(u -> (new Random().nextInt(6) == 5) ? 1 : 0).toArray(), s, s+12)).sum() >= 2) ? 1 : 0) : 0).sum()/100d;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        Random rand = new Random();
-        int count = 0;
-        for (int i = 0; i < 10000; i++) {
-            int x = 0;
-            for (int j = 0; j < 18; j++) {
-                if (1+rand.nextInt(6) == 6)
-                    x++;
-            }
-            if (x >= 3) {
-                count++;
+        int count = 0, x = 0;
+        for (int i = 0; i < 180000; i++) {
+            if (new Random().nextInt(6) == 5) x++;
+            if (i % 18 == 0) {
+                if (x > 2) count++;
+                x = 0;
             }
         }
         return count/100d;
+        //The line below contains a one line version that may or may not work. It is not recommended to use this version due to extreme inefficiencies
+        //return IntStream.range(0, 180000).boxed().mapToInt(s -> (s % 6 == 0) ? ((Arrays.stream(Arrays.copyOfRange(IntStream.range(0, 180000).boxed().mapToInt(u -> (new Random().nextInt(6) == 5) ? 1 : 0).toArray(), s, s+18)).sum() >= 3) ? 1 : 0) : 0).sum()/100d;
     }
 
 
